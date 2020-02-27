d40ce30633bc7afd04292879dc3e9fc9aeadae9a
zinnourlahcene <zinnour@uqtr.ca>
2020-02-27 13:17:22 -0500

    fitbit api

--- src/main/java/ca/uqtr/fitbit/api/data/steps/StepsImpl.java
+++ src/main/java/ca/uqtr/fitbit/api/data/steps/StepsImpl.java
@@ -73,7 +73,7 @@ public class StepsImpl implements Steps {
         String json;
         //https://api.fitbit.com/1/user/-/activities/steps/date/2020-01-20/1d/1min/time/08%3A00/12%3A00.json
         Request request = new Request.Builder()
-                .url("https://api.fitbit.com/1/user/-/activities/steps/date/"+date+"/1d/1min/time/"+java.net.URLEncoder.encode(startTime, StandardCharsets.UTF_8)+"/"+java.net.URLEncoder.encode(endTime, StandardCharsets.UTF_8)+".json")
+                .url("https://api.fitbit.com/1/user/-/activities/steps/date/"+date+"/1d/1min/time/"+startTime+"/"+endTime+".json")
                 .get()
                 .header("Authorization", "Bearer "+accessToken)
                 .build();
