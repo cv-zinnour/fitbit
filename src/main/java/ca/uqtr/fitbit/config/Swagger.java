1bcf0f5cf4478faa41998bab8ba0658b44cfcbef
zinnourlahcene <zinnour@uqtr.ca>
2020-02-27 13:22:08 -0500

    fitbit api

--- src/main/java/ca/uqtr/fitbit/config/Swagger.java
+++ src/main/java/ca/uqtr/fitbit/config/Swagger.java
@@ -1,62 +1,62 @@
-//package ca.uqtr.fitbit.config;
-//
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
-//import springfox.documentation.builders.ApiInfoBuilder;
-//import springfox.documentation.builders.PathSelectors;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.service.ApiInfo;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spring.web.plugins.Docket;
-//import springfox.documentation.swagger.web.UiConfiguration;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
-//
-//
-//@Configuration
-//@Controller
-//@EnableSwagger2
-//public class Swagger {
-//
-//    @RequestMapping(value = "/docs", method = RequestMethod.GET)
-//    public String docs(){
-//        return "redirect:/swagger-ui.html";
-//    }
-//
-//    @Bean
-//    public Docket swaggerApi(){
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .groupName("auth-api")
-//                .apiInfo(swaggerApiInfo())
-//                .select()
-//                .apis(RequestHandlerSelectors.any())
-//                .paths(PathSelectors.any())
-//                .build();
-//    }
-//
-//    private ApiInfo swaggerApiInfo() {
-//        return new ApiInfoBuilder()
-//                .title("Fitbit microservice")
-//                .description("Fitbit microservice for ePod app.")
-//                .version("1.0.0")
-//                .build();
-//    }
-//
-//    @Bean
-//    UiConfiguration uiConfig() {
-//        return new UiConfiguration(
-//                null,
-//                "none",
-//                "alpha",
-//                "schema",
-//                UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS,
-//                false,
-//                true,
-//                60000L);
-//    }
-//
-//
-//}
+package ca.uqtr.fitbit.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+@Configuration
+@Controller
+@EnableSwagger2
+public class Swagger {
+
+    @RequestMapping(value = "/docs", method = RequestMethod.GET)
+    public String docs(){
+        return "redirect:/swagger-ui.html";
+    }
+
+    @Bean
+    public Docket swaggerApi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("patient-api")
+                .apiInfo(swaggerApiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo swaggerApiInfo() {
+        return new ApiInfoBuilder()
+                .title("Patient microservice")
+                .description("Patient microservice for ePod app.")
+                .version("1.0.0")
+                .build();
+    }
+
+    @Bean
+    UiConfiguration uiConfig() {
+        return new UiConfiguration(
+                null,
+                "none",
+                "alpha",
+                "schema",
+                UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS,
+                false,
+                true,
+                60000L);
+    }
+
+
+}
