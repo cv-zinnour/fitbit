29be03bb940740d23875bc3b61f5702f09f096f2
zinnourlahcene <zinnour@uqtr.ca>
2020-02-28 14:31:47 -0500

    fitbit api

--- src/main/java/ca/uqtr/fitbit/service/device/DeviceServiceImpl.java
+++ src/main/java/ca/uqtr/fitbit/service/device/DeviceServiceImpl.java
@@ -160,7 +160,7 @@ public class DeviceServiceImpl implements DeviceService {
     public Response readAvailableDevicesByInstitutionCode(DeviceDto device) {
         try{
             Type deviceDtoList = new TypeToken<List<DeviceDto>>() {}.getType();
-            return new Response(modelMapper.map(deviceRepository.findAllByInstitutionCodeAndAvailable(UUID.fromString(device.getInstitutionCode()), true), deviceDtoList), null);
+            return new Response(modelMapper.map(deviceRepository.findAllByInstitutionCodeAndAvailable(device.getInstitutionCode(), true), deviceDtoList), null);
         } catch (Exception ex){
             LOGGER.log( Level.WARNING, ex.getMessage());
             return new Response(null,
