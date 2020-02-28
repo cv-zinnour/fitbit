775f6ac79335781b2b5cc1c585db73634fd36ef0
zinnourlahcene <zinnour@uqtr.ca>
2020-02-28 16:00:37 -0500

    fitbit api

--- src/main/java/ca/uqtr/fitbit/service/device/DeviceServiceImpl.java
+++ src/main/java/ca/uqtr/fitbit/service/device/DeviceServiceImpl.java
@@ -79,12 +79,19 @@ public class DeviceServiceImpl implements DeviceService {
 
     @Override
     public Response updateDevice(DeviceDto device) {
-        System.out.println(device.toString());
-        return new Response(modelMapper.map(deviceRepository.save(device.dtoToObj(modelMapper)), DeviceDto.class), null);
+        try{
+            return new Response(modelMapper.map(deviceRepository.save(device.dtoToObj(modelMapper)), DeviceDto.class), null);
+        } catch (Exception ex){
+            LOGGER.log( Level.WARNING, ex.getMessage());
+            return new Response(null,
+                    new Error(Integer.parseInt(messageSource.getMessage("error.null.id", null, Locale.US)),
+                            messageSource.getMessage("error.null.message", null, Locale.US)));
+        }
     }
 
     @Override
     public Response readDevices(DeviceDto device) {
+        System.out.println(device.toString());
         try{
             Type deviceDtoList = new TypeToken<List<DeviceDto>>() {}.getType();
             return new Response(modelMapper.map(deviceRepository.findAllByAdminId(UUID.fromString(device.getId())), deviceDtoList), null);
