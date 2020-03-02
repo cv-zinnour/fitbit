c35eeabe7a8feaf2ee3f56514966aa682a54e98e
zinnourlahcene <zinnour@uqtr.ca>
2020-03-01 20:32:53 -0500

    fitbit api

--- src/main/java/ca/uqtr/fitbit/config/Config.java
+++ src/main/java/ca/uqtr/fitbit/config/Config.java
@@ -6,13 +6,14 @@ import lombok.var;
 import okhttp3.OkHttpClient;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.context.support.ResourceBundleMessageSource;
 
 @Configuration
 public class Config {
-
 
     @Bean
     OkHttpClient okHttpClient(){
@@ -23,11 +24,24 @@ public class Config {
 
     @Bean
     public ResourceBundleMessageSource messageResourceSource() {
+
         ResourceBundleMessageSource source = new ResourceBundleMessageSource();
-        source.setBasenames("messages");
+        source.setBasenames("classpath:messages");
         source.setUseCodeAsDefaultMessage(true);
+
         return source;
     }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource
+                = new ReloadableResourceBundleMessageSource();
+
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
 
     @Bean
     public ModelMapper modelMapper() {
