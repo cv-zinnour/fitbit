59d58ccfaa84760611cd0b7ddd4a9e01812b4f0a
zinnourlahcene <zinnour@uqtr.ca>
2020-03-03 15:53:06 -0500

    fitbit api

--- src/main/java/ca/uqtr/fitbit/controller/DeviceController.java
+++ src/main/java/ca/uqtr/fitbit/controller/DeviceController.java
@@ -153,7 +153,7 @@ public class DeviceController {
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @PostMapping("/subscriptions")
+    @PostMapping("/notifications")
     public ResponseEntity<HttpStatus> getFitBitNotificationData() {
         System.out.println("////////////////////////  getFitBitNotificationData");
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
