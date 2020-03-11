e5be89afa62e19d66353876af420731964bd8360
zinnourlahcene <zinnour@uqtr.ca>
2020-03-11 11:59:11 -0400

    fitbit api

--- src/main/java/ca/uqtr/fitbit/api/LoggingInterceptor.java
+++ src/main/java/ca/uqtr/fitbit/api/LoggingInterceptor.java
@@ -15,14 +15,17 @@ public class LoggingInterceptor implements Interceptor {
         Request request = chain.request();
 
         long t1 = System.nanoTime();
-        logger.info(String.format("Sending request: %s \nBody: %s \nHeaders: %s",
-                request.url(), request.body(), request.headers()));
+//        logger.info(String.format("Sending request: %s \nBody: %s \nHeaders: %s",
+//                request.url(), request.body(), request.headers()));
+        logger.info("Sending request:  " + request.url());
 
         Response response = chain.proceed(request);
 
         long t2 = System.nanoTime();
-        logger.info(String.format("Received response for %s in %.1fms%n%s",
-                response.request().url(), (t2 - t1) / 1e6d, response.headers()));
+//        logger.info(String.format("Received response for %s in %.1fms%n%s",
+//                response.request().url(), (t2 - t1) / 1e6d, response.headers()));
+        logger.info(String.format("Received response for %s in %.1fms%n",
+                response.request().url(), (t2 - t1) / 1e6d));
 
         return response;
     }
