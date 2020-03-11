644701a3129c61ed61b1d1a10c4dac85a6efe48f
zinnourlahcene <zinnour@uqtr.ca>
2020-03-11 15:38:57 -0400

    fitbit api

--- src/main/java/ca/uqtr/fitbit/service/activity/ActivityServiceImpl.java
+++ src/main/java/ca/uqtr/fitbit/service/activity/ActivityServiceImpl.java
@@ -70,8 +70,6 @@ public class ActivityServiceImpl implements ActivityService {
         System.out.println("////////////////////activitiesSteps   "+activitiesSteps.getDateTime().toString());
         System.out.println("////////////////////activitiesCalories   "+activitiesCalories.getDateTime().toString());
         System.out.println("////////////////////activitiesDistance   "+activitiesDistance.getDateTime().toString());
-        Device device = deviceRepository.getDeviceById(deviceDto.getId());
-        System.out.println("////////////////////   "+device.toString());
 
     }
 
