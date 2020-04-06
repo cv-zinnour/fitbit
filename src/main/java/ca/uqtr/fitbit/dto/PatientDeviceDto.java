ac9063550bbcea30b005923ea1f144fdbdae3cf0
zinnourlahcene <zinnour@uqtr.ca>
2020-04-06 11:15:50 -0400

    fitbit api

--- src/main/java/ca/uqtr/fitbit/dto/PatientDeviceDto.java
+++ src/main/java/ca/uqtr/fitbit/dto/PatientDeviceDto.java
@@ -41,10 +41,4 @@ public class PatientDeviceDto {
             return null;
     }
 
-    public UUID getMedicalFileId() {
-        if (this.medicalFileId != null)
-            return UUID.fromString(this.medicalFileId);
-        else
-            return null;
-    }
 }
