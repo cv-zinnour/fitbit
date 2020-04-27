d251d5519888242a08bc299c432f0adba747ce02
zinnourlahcene <zinnour@uqtr.ca>
2020-04-27 11:33:27 -0400

    sub id

--- src/main/java/ca/uqtr/fitbit/service/device/DeviceServiceImpl.java
+++ src/main/java/ca/uqtr/fitbit/service/device/DeviceServiceImpl.java
@@ -309,12 +309,12 @@ public class DeviceServiceImpl implements DeviceService {
             long d1 = device1.get().getLastSyncDate().getTime();
             System.out.println("//////////////////////// 2 ");
             //TODO Delete - TimeUnit.MINUTES.toMillis(240)
-            long minutes = TimeUnit.MILLISECONDS.toMinutes(cal.getTime().getTime() - d1 - TimeUnit.MINUTES.toMillis(240));
+            long minutes = TimeUnit.MILLISECONDS.toMinutes(cal.getTime().getTime() - d1 /*- TimeUnit.MINUTES.toMillis(240)*/);
 
             System.out.println("//////////////////////// 3 ");
             int j = (int) (minutes/1440);
             System.out.println("//////////////////////// 4 ");
-            long d2 = cal.getTime().getTime() - TimeUnit.MINUTES.toMillis(1) - TimeUnit.MINUTES.toMillis(240);
+            long d2 = cal.getTime().getTime() - TimeUnit.MINUTES.toMillis(1) /*- TimeUnit.MINUTES.toMillis(240)*/;
 
             System.out.println("//////////////////////// 5 ");
             if (j > 0)
