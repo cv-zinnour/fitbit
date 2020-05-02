d8e577f4223c5559e508563bdb3f8ee285d3021d
zinnourlahcene <zinnour@uqtr.ca>
2020-05-02 02:37:20 -0400

    sub id

--- src/main/java/ca/uqtr/fitbit/repository/PatientDeviceRepository.java
+++ src/main/java/ca/uqtr/fitbit/repository/PatientDeviceRepository.java
@@ -14,7 +14,7 @@ public interface PatientDeviceRepository extends CrudRepository<PatientDevice, U
     @Query("select pd from PatientDevice pd where pd.device.id = :deviceId and pd.returnedAt is null")
     PatientDevice getByDeviceIdAndReturnedAtIsNull(UUID deviceId);
 
-    @Query("select sum(steps.value) from PatientDevice pd left join fetch pd.activitiesSteps steps where pd.id = steps.patient_device_id and pd.medicalFileId = :medicalFileId and steps.dateTime between :date1 and :date2 group by steps.dateTime")
+    @Query("select sum(steps.value) from PatientDevice pd left join fetch pd.activitiesSteps steps where pd.id = steps.patientDevice.id and pd.medicalFileId = :medicalFileId and steps.dateTime between :date1 and :date2 group by steps.dateTime")
     int getStepsBetweenTwoVisits(String medicalFileId, Date date1, Date date2);
 
 }
