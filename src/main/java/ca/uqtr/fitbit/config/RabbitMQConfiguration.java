b4bada8f061db1dcff855965bac011d17bcdcf8b
zinnourlahcene <zinnour@uqtr.ca>
2021-04-27 06:11:12 -0400

    rabbitmq

--- src/main/java/ca/uqtr/fitbit/config/RabbitMQConfiguration.java
+++ src/main/java/ca/uqtr/fitbit/config/RabbitMQConfiguration.java
@@ -14,6 +14,52 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitMQConfiguration {
 
+    @Value("${rabbitmq.exchange}")
+    private String exchange;
+    @Value("${rabbitmq.dead-letter-exchange}")
+    private String deadLetterExchange;
+
+    @Value("${rabbitmq.queue}")
+    private String queue;
+    @Value("${rabbitmq.dead-letter-queue}")
+    private String deadLetterQueue;
+
+    @Value("${rabbitmq.routing-key}")
+    private String routingKey;
+    @Value("${rabbitmq.dlq-routing-key}")
+    private String DLQRoutingKey;
+
+
+    @Bean
+    DirectExchange exchange() {
+        return new DirectExchange(exchange);
+    }
+    @Bean
+    DirectExchange DLX() {
+        return new DirectExchange("dead-letter-exchange");
+    }
+
+    @Bean
+    Queue queue() {
+        return QueueBuilder.durable(queue)
+                .withArgument("x-dead-letter-exchange", deadLetterExchange)
+                .withArgument("x-dead-letter-routing-key", deadLetterQueue)
+                .build();
+    }
+    @Bean
+    Queue DLQ() {
+        return QueueBuilder.durable(deadLetterQueue).build();
+    }
+
+    @Bean
+    Binding binding() {
+        return BindingBuilder.bind(queue()).to(exchange()).with(routingKey);
+    }
+    @Bean
+    Binding DLQBinding() {
+        return BindingBuilder.bind(DLQ()).to(DLX()).with(DLQRoutingKey);
+    }
+
     @Bean
     public Jackson2JsonMessageConverter jsonMessageConverter() {
         return new Jackson2JsonMessageConverter();
