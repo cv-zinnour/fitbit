da44bbd12945d57dcc7c25f841772bc1e7652b87
zinnourlahcene <zinnour@uqtr.ca>
2022-01-07 02:08:31 -0500

    Steps update

--- src/main/java/ca/uqtr/fitbit/controller/ActivityController.java
+++ src/main/java/ca/uqtr/fitbit/controller/ActivityController.java
@@ -69,7 +69,7 @@ public class ActivityController {
         return activityService.getCaloriesOfDayBetweenTwoTimesPerMinuteFromApi(date, startTime, endTime, deviceDto);
     }
 
-    @GetMapping(value = "/activity/steps")
+    @PostMapping(value = "/activity/steps")
     @ResponseBody
     public Response getStepsPerVisits(@RequestParam String medicalFileId,
                                      @RequestBody Request request) throws IOException {
@@ -78,7 +78,7 @@ public class ActivityController {
         return activityService.getStepsPerVisits(medicalFileId, dates);
     }
 
-    @GetMapping(value = "/activity/activeminutes")
+    @PostMapping(value = "/activity/activeminutes")
     @ResponseBody
     public Response getActiveMinutesPerVisits(@RequestParam String medicalFileId,
                                        @RequestBody Request request) throws IOException {
